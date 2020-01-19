21461853c266718a25ef0c8746d8f2cd20589daf
Janielle Gregorio <hello_janielle1992@yahoo.com.ph>
2020-01-19 21:41:48 +0800

    Updated basics java file

--- REST-ASSURED_Tests/src/basics.java
+++ REST-ASSURED_Tests/src/basics.java
@@ -1,8 +1,26 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import static io.restassured.RestAssured.given;
+
 
 public class basics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		RestAssured.baseURI = "https://maps.googleapis.com";
+		
+		given().
+				param("location","-33.8670522,151.1957362").
+				param("radius","500").
+				param("type","restaurant").
+				param("keyword","cruise").
+				param("key","AIzaSyDxeUgptccx5i2Y0HRQZzCpd1N2ew8t43c").
+		when().
+				get("/maps/api/place/nearbysearch/json").
+		then().
+				assertThat().statusCode(200).and().contentType(ContentType.JSON);
+		
 
 	}
 
